6da6b05a4ff56277a2c16e95158da29c092c7562
Ian Cahn <iancahn@live.com>
2022-08-13 17:43:03 +0200

    Passing objects as props now
    To keep the card component property passing down to a minimum, we gave the Card component the entire item object.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Card(props) {
     let badgeText;
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT";
-    } else if (props.location === "Online") {
+    } else if (props.item.location === "Online") {
         badgeText = "ONLINE";
     }
 
@@ -12,16 +12,16 @@ function Card(props) {
     return (
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={`../images/${props.img}`} alt="" className="card--image" />
+            <img src={`../images/${props.item.coverImg}`} alt="" className="card--image" />
             <div className='card--stats'>
                 {/* stars pngs */}
                 <img src='../images/star.png' className="card--star" alt="the star rating" />
-                <span>{props.rating}</span>
-                <span className='gray'>({props.reviewCount}) • </span>
-                <span className='gray'>{props.country}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className='gray'>({props.item.stats.reviewCount}) • </span>
+                <span className='gray'>{props.item.country}</span>
             </div>
-            <p className="card--title">{props.title}</p>
-            <p className="card--price"><span className='bold'>From ${props.price} per night</span></p>
+            <p className="card--title">{props.item.title}</p>
+            <p className="card--price"><span className='bold'>From ${props.item.price} per night</span></p>
         </div>
     )
 }
